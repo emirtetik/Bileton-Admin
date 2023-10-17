40fe44d6a256488f0ed1293740ae336d42dd3df8
Emir Tetik <info@frontmir.com>
2023-10-17 19:52:28 +0300

    router-dom ve tailwind paketleri projeye dahil edildi

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+
+const Home = () => {
+  return (
+    <div className="text-3xl bg-red-950 font-extrabold text-white py-4 flex justify-center items-center "> 
+       Hoşgeldin!!
+    </div>
+  )
+}
+
+export default Home
