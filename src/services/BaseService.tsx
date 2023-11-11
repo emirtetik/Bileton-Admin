5296fe0d5617a0535feff7ce0539416899d22ccc
Nedim <mr.ramicevic@gmail.com>
2023-11-11 16:30:51 +0300

    api url

--- src/services/BaseService.tsx
+++ src/services/BaseService.tsx
@@ -1,4 +1,4 @@
-const BASE_API_URL = "http://localhost:8080/api/event/";
+const BASE_API_URL = "https://event-api-tqwv.onrender.com/api/event/";
 
 export const BaseService = {
   get: async (path: string) => {
